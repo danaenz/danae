181c6139fcf86582e6d37e07e1b1d3e905078082
Danae Miller-Clendon <danae@infinity.io>
2014-06-14 01:10:36 +1200

    Initial setup

---
+++ config.rb
@@ -0,0 +1,5 @@
+http_path = "/"
+css_dir = "stylesheets/css"
+sass_dir = "stylesheets/sass"
+images_dir = "images"
+javascripts_dir = "js/custom"
--- gruntfile.js
+++ gruntfile.js
@@ -1,3 +1,55 @@
-/**
- * Created by Danae on 14/06/14.
- */
+module.exports = function(grunt) {
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+
+    compass: {
+      dist: {
+        options: {
+          config: 'config.rb'
+        }
+      }
+    },
+    concat: {
+      options: {
+        // define a string to put between each file in the concatenated output
+        separator: ';'
+      },
+      dist: {
+        src: ['js/custom/*.js'],
+        dest: 'js/main.js'
+      }
+    },
+    uglify: {
+      options: {
+        banner: '/*! <%= pkg.name %> || <%= grunt.template.today("dd-mm-yyyy") %> */\n'
+      },
+      dist: {
+        files: {
+          'js/main.min.js': ['<%= concat.dist.dest %>']
+        }
+      }
+    },
+    watch: {
+      css: {
+        files: [
+          'stylesheets/**/*.scss'
+        ],
+        tasks: ['compass']
+      },
+      concat: {
+        files: ['js/custom/*.js'],
+        tasks: ['concat', 'uglify']
+      }
+    }
+
+  });
+
+
+  grunt.loadNpmTasks('grunt-contrib-compass');
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+
+// Default task(s).
+  grunt.registerTask('default', ['compass', 'concat', 'uglify', 'watch']);
+};
